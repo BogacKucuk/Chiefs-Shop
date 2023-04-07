49506dbb2e07b3fe9a703886d37892590f84c0cb
Bogac Kucuk <bogackucuk@outlook.com>
2023-04-07 15:13:21 +0300

    cart context changes: -decrease (remove) item from cart func added

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState, useEffect } from "react";
 
-// helper function
+// helper functions
+
 const addCartItem = (cartItems, productToAdd) => {
   // find if cartItems contains productToAdd
   const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id);
@@ -17,11 +18,28 @@ const addCartItem = (cartItems, productToAdd) => {
   return [...cartItems, { ...productToAdd, quantity: 1 }]
 }
 
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  // find the cart item to remove
+  const existingCartItem = cartItems.find((cartItem) => cartItem.id === cartItemToRemove.id);
+
+  // check if quantity is equal to 1, if it is remove that item form the cart
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+
+  // return back cart items with matching cart item with reduced quantity
+  return cartItems.map((cartItem) => cartItem.id === cartItemToRemove.id
+    ? { ...cartItem, quantity: cartItem.quantity - 1 }
+    : cartItem
+  );
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => { },
   cartItems: [],
   addItemToCart: () => { },
+  removeItemFromCart: () => { },
   cartCount: 0
 });
 
@@ -39,7 +57,18 @@ export const CartProvider = ({ children }) => {
     return setCartItems(addCartItem(cartItems, productToAdd));
   }
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount };
+  const removeItemFromCart = (cartItemToRemove) => {
+    return setCartItems(removeCartItem(cartItems, cartItemToRemove));
+  }
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart, 
+    removeItemFromCart,
+    cartItems,
+    cartCount
+  };
   return (
     <CartContext.Provider value={value}>{children}</CartContext.Provider>
   )

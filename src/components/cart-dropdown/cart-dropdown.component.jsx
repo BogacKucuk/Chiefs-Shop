bfa58bdad0eb3152c278a1900de96549ca9c056c
Bogac Kucuk <bogackucuk@outlook.com>
2023-04-07 15:11:40 +0300

    cart dropdown navigate to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,6 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
 import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../button/button.component";
@@ -8,6 +10,12 @@ import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  }
+
   return (
     <div className="cart-dropdown-container">
       <div className="card-items">
@@ -17,7 +25,7 @@ const CartDropdown = () => {
           ))
         }
       </div>
-      <Button>CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
     </div>
   )
 

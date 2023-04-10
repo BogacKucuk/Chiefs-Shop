40cafa47c14fef5eba2e78ae95ed9115e40cb210
Bogac Kucuk <bogackucuk@outlook.com>
2023-04-10 15:58:36 +0300

    directory-item navigation added categories const variable moved

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,11 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
 import { DirectoryContainer, Body, BackgroundImage } from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
+    const { title, imageUrl, route } = category;
+    const navigate = useNavigate();
 
-    const { title, imageUrl } = category;
+    const onNavigateHandler = () => navigate(route);
 
     return (
-        <DirectoryContainer>
+        <DirectoryContainer onClick={onNavigateHandler}>
             <BackgroundImage imageUrl={imageUrl} />
             <Body>
                 <h2>{title}</h2>

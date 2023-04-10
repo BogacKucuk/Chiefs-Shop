6b512ef9bc2b7209c76a2c99ffd32aa374e1ef25
Bogac Kucuk <bogackucuk@outlook.com>
2023-04-10 15:47:24 +0300

    all components uses stlyed components

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,10 +1,11 @@
-import { useContext, useState, useEffect } from "react";
-import { CategoriesContext } from "../../contexts/categories.context";
+import { useContext, useState, useEffect, Fragment } from 'react';
+import { useParams } from 'react-router-dom';
 
-import { useParams } from "react-router-dom";
-import ProductCard from "../../components/product-card/product-card.component";
+import ProductCard from '../../components/product-card/product-card.component';
 
-import "./category.styles.scss";
+import { CategoriesContext } from '../../contexts/categories.context';
+
+import { CategoryContainer, Title } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
@@ -13,21 +14,19 @@ const Category = () => {
 
   useEffect(() => {
     setProducts(categoriesMap[category]);
-  }, [category, categoriesMap])
+  }, [category, categoriesMap]);
 
   return (
-    <>
-      <h2 className="category-title">{category.toUpperCase()}</h2>
-      <div className="category-container">
-        {
-          products &&
+    <Fragment>
+      <Title>{category.toUpperCase()}</Title>
+      <CategoryContainer>
+        {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
-          ))
-        }
-      </div>
-    </>
-  )
-}
+          ))}
+      </CategoryContainer>
+    </Fragment>
+  );
+};
 
 export default Category;
